d5f5b9366827ac122a6a2eccb8ae91ff8988c20c
sanghun0724 <sanghun0724@naver.com>
2021-12-27 17:59:18 +0900

    중복된 코드 (로직) 구현 부분 줄이기 적용

--- StockTutorial/StockTutorial/Repository/StockRepository.swift
+++ StockTutorial/StockTutorial/Repository/StockRepository.swift
@@ -8,5 +8,6 @@
 import Combine
 
 protocol StockRepository {
-    func fetchStockPublisher(keywords:String) -> AnyPublisher<StockResult,Error> 
+    func fetchStockPublisher(keywords:String) -> AnyPublisher<StockResult,Error>
+    func fetchTimeSeriesPublish(keywords:String) -> AnyPublisher<TimeSeriesMonthlyAdjusted,Error> 
 }
--- StockTutorial/StockTutorial/Repository/StockRepositoryImpl.swift
+++ StockTutorial/StockTutorial/Repository/StockRepositoryImpl.swift
@@ -21,35 +21,47 @@ class StockRepositoryImpl:StockRepository {
     
     //URLString은 띄어쓰기가 포함안됨 -> 띄어쓰기가 올것을 대비해 encoding하는 작업이 필요
     func fetchStockPublisher(keywords: String) -> AnyPublisher<StockResult, Error> {
-        guard let keywords = keywords.addingPercentEncoding(withAllowedCharacters: .urlHostAllowed) else { //띄어쓰기 처리
-            let error:StockRepositoryError = .encoding
+        //1 parseQuerySting
+        let queryResult = parseqQueryString(text: keywords)
+        var query = ""
+        switch queryResult {
+        case .failure(let error):
             return Fail(error: error).eraseToAnyPublisher()
+        case .success(let value):
+            query = value
         }
         
-        let urlString = "https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=\(keywords)&apikey=\(apiKey)"
+        //2 parseURL
+        let urlResult = parseURL(urlString:  "https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=\(query)&apikey=\(apiKey)")
         
-        guard let url = URL(string: urlString) else {
-            let error = URLError(.badURL)
+        switch urlResult {
+        case .failure(let error):
             return Fail(error: error).eraseToAnyPublisher()
+        case .success(let url):
+            return URLSession.shared.dataTaskPublisher(for: url).map { $0.data }.decode(type: StockResult.self, decoder: decoder).receive(on: RunLoop.main).eraseToAnyPublisher()
         }
-        
-        return URLSession.shared.dataTaskPublisher(for: url).map { $0.data }.decode(type: StockResult.self, decoder: decoder).receive(on: RunLoop.main).eraseToAnyPublisher()
     }
     
     func fetchTimeSeriesPublish(keywords:String) -> AnyPublisher<TimeSeriesMonthlyAdjusted,Error> {
-        guard let keywords = keywords.addingPercentEncoding(withAllowedCharacters: .urlHostAllowed) else {
-            let error:StockRepositoryError = .encoding
+        //1
+        let queryResult = parseqQueryString(text: keywords)
+        var query = ""
+        
+        switch queryResult {
+        case .failure(let error):
             return Fail(error: error).eraseToAnyPublisher()
+        case .success(let value):
+            query = value
         }
         
-        let urlString = "https://www.alphavantage.co/query?function=TIME_SERIES_MONTHLY_ADJUSTED&symbol=\(keywords)&apikey=\(apiKey)"
-        guard let url = URL(string: urlString) else {
-            let error:StockRepositoryError = .bad_url
+        let urlResult = parseURL(urlString: "https://www.alphavantage.co/query?function=TIME_SERIES_MONTHLY_ADJUSTED&symbol=\(query)&apikey=\(apiKey)")
+        
+        switch urlResult {
+        case .failure(let error):
             return Fail(error: error).eraseToAnyPublisher()
+        case .success(let url):
+            return URLSession.shared.dataTaskPublisher(for: url).map { $0.data }.decode(type: TimeSeriesMonthlyAdjusted.self, decoder: decoder).receive(on: RunLoop.main).eraseToAnyPublisher()
         }
-        
-        return URLSession.shared.dataTaskPublisher(for: url).map { $0.data }.decode(type: TimeSeriesMonthlyAdjusted.self, decoder: decoder).receive(on: RunLoop.main).eraseToAnyPublisher()
-        
     }
     
     private func parseURL(urlString:String) -> Result<URL,Error> {
@@ -60,8 +72,6 @@ class StockRepositoryImpl:StockRepository {
             return .failure(error)
         }
     }
-     
-    
     
     private func parseqQueryString(text:String) -> Result<String,Error> {
         if let query = text.addingPercentEncoding(withAllowedCharacters: .urlHostAllowed) {
@@ -71,11 +81,7 @@ class StockRepositoryImpl:StockRepository {
             return .failure(error)
         }
     }
-    
-    
-    
-    
-    
-    
-    
 }
+
+
+//이것도 코드중복아니야 ? nono 왜냐하면 우리가 중복되게 하지 않으려는건 Business Logic임 로직을 단순화시키고 수정을 손쉽게 만듬 템플릿코드같은게 중복되는거는 쩔수없는거고
