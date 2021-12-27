d3b3f42208160db65a25c8e642f1dd622a5b76ae
sanghun0724 <sanghun0724@naver.com>
2021-12-27 17:44:26 +0900

    중복된 코드 제거 적용 전

---
+++ StockTutorial/StockTutorial/Model/TimeSeriesMonthlyAdjusted.swift
@@ -0,0 +1,42 @@
+//
+//  TimeSeriesMonthlyAdjusted.swift
+//  StockTutorial
+//
+//  Created by sangheon on 2021/12/27.
+//
+
+import Foundation
+
+struct TimeSeriesMonthlyAdjusted: Decodable {
+    let meta: Meta
+    let series:[String:OHLC]
+    
+    enum CodingKeys:String, CodingKey {
+        case meta = "Meta Data"
+        case series = "Monthly Adjusted Time Series"
+    }
+    
+    struct OHLC:Decodable {
+        let open:String
+        let close:String
+        let adjustedClose:String
+        
+        enum CodingKeys:String,CodingKey {
+            case open = "1. open"
+            case close = "4. colse"
+            case adjustedClose = "5. adjusted close"
+        }
+    }
+    
+    struct Meta: Decodable {
+        let symbol:String
+        
+        enum CodingKeys:String,CodingKey {
+            case symbol = "2. Symbol"
+        }
+    }
+    
+    
+    
+    
+}
--- StockTutorial/StockTutorial/Repository/StockRepositoryImpl.swift
+++ StockTutorial/StockTutorial/Repository/StockRepositoryImpl.swift
@@ -10,10 +10,22 @@ import Combine
 
 class StockRepositoryImpl:StockRepository {
     
+    enum StockRepositoryError: Error{
+        case encoding
+        case bad_url
+    }
+    
+    
     let apiKey:String = "H4ZRG44U4X1T6VSV"
     let decoder = JSONDecoder()
     
+    //URLString은 띄어쓰기가 포함안됨 -> 띄어쓰기가 올것을 대비해 encoding하는 작업이 필요
     func fetchStockPublisher(keywords: String) -> AnyPublisher<StockResult, Error> {
+        guard let keywords = keywords.addingPercentEncoding(withAllowedCharacters: .urlHostAllowed) else { //띄어쓰기 처리
+            let error:StockRepositoryError = .encoding
+            return Fail(error: error).eraseToAnyPublisher()
+        }
+        
         let urlString = "https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=\(keywords)&apikey=\(apiKey)"
         
         guard let url = URL(string: urlString) else {
@@ -23,4 +35,47 @@ class StockRepositoryImpl:StockRepository {
         
         return URLSession.shared.dataTaskPublisher(for: url).map { $0.data }.decode(type: StockResult.self, decoder: decoder).receive(on: RunLoop.main).eraseToAnyPublisher()
     }
+    
+    func fetchTimeSeriesPublish(keywords:String) -> AnyPublisher<TimeSeriesMonthlyAdjusted,Error> {
+        guard let keywords = keywords.addingPercentEncoding(withAllowedCharacters: .urlHostAllowed) else {
+            let error:StockRepositoryError = .encoding
+            return Fail(error: error).eraseToAnyPublisher()
+        }
+        
+        let urlString = "https://www.alphavantage.co/query?function=TIME_SERIES_MONTHLY_ADJUSTED&symbol=\(keywords)&apikey=\(apiKey)"
+        guard let url = URL(string: urlString) else {
+            let error:StockRepositoryError = .bad_url
+            return Fail(error: error).eraseToAnyPublisher()
+        }
+        
+        return URLSession.shared.dataTaskPublisher(for: url).map { $0.data }.decode(type: TimeSeriesMonthlyAdjusted.self, decoder: decoder).receive(on: RunLoop.main).eraseToAnyPublisher()
+        
+    }
+    
+    private func parseURL(urlString:String) -> Result<URL,Error> {
+        if let url = URL(string: urlString) {
+            return .success(url)
+        } else  {
+            let error:StockRepositoryError = .bad_url
+            return .failure(error)
+        }
+    }
+     
+    
+    
+    private func parseqQueryString(text:String) -> Result<String,Error> {
+        if let query = text.addingPercentEncoding(withAllowedCharacters: .urlHostAllowed) {
+            return .success(query)
+        } else {
+            let error:StockRepositoryError = .encoding
+            return .failure(error)
+        }
+    }
+    
+    
+    
+    
+    
+    
+    
 }
