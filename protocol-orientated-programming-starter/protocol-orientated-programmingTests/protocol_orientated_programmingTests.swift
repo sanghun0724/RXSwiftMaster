4ed09658b5c6a017cc436b658c8843e8aa4d2f4d
sanghun0724 <sanghun0724@naver.com>
2022-01-05 23:29:04 +0900

    unit test done

--- protocol-orientated-programming-starter/protocol-orientated-programmingTests/protocol_orientated_programmingTests.swift
+++ protocol-orientated-programming-starter/protocol-orientated-programmingTests/protocol_orientated_programmingTests.swift
@@ -10,9 +10,9 @@ import XCTest
 
 class protocol_orientated_programmingTests: XCTestCase {
     
-    private var sut:UserViewModel!
-    private var userService:MockUserService!
-    private var output: MockUserViewOutput!
+    private var sut:UserViewModel! // 로직단 타입 가져오고
+    private var userService:MockUserService! // protocol impl
+    private var output: MockUserViewOutput! //mock들
 
     override func setUpWithError() throws {
         output = MockUserViewOutput()
@@ -56,7 +56,7 @@ class protocol_orientated_programmingTests: XCTestCase {
 
 
 class MockUserService:UserService {
-    var fetchUserMockResult:Result<User, Error>?
+    var fetchUserMockResult:Result<User, Error>? //테스트할때 위에서 success , failure 나누려고
     
     func fetchUser(completion: @escaping (Result<User, Error>) -> Void) {
         if let result = fetchUserMockResult {
@@ -65,7 +65,7 @@ class MockUserService:UserService {
     }
 }
 
-class MockUserViewOutput: UserViewModelOutput {
+class MockUserViewOutput: UserViewModelOutput { //차피 앱실행될때 뷰컨에서 뷰딛로드에서 실행되니.. 업데이트뷰 func가
     var updateViewArray:[(imageUrl:String,email:String)] = []
     // i want read image url ,email <- Func 할떄마다
     func updateView(imageUrl: String, email: String) {
