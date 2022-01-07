cc8b0f32694de85918b193c34984ca6f26b6a9f7
sanghun0724 <sanghun0724@naver.com>
2022-01-07 22:12:17 +0900

    add unit test 2  testShowLogin_whenLoginStorageReturnsEmptyUserToken

---
+++ protocol-orientated-programming-starter/protocol-orientated-programmingTests/RootViewModelTest.swift
@@ -0,0 +1,94 @@
+//
+//  RootViewModelTest.swift
+//  protocol-orientated-programmingTests
+//
+//  Created by sangheon on 2022/01/07.
+//
+
+import XCTest
+@testable import protocol_orientated_programming
+
+class RootViewModelTest: XCTestCase {
+    
+    private var sut:RootViewModel!  // sut?  <- what we test for
+    private var userService:MockUserService!
+    private var loginStorageService:MockLoginStorageService!
+    private var output:MockRootViewModelOutput!
+    
+    override func setUpWithError() throws {
+        
+        userService = MockUserService()
+        loginStorageService = MockLoginStorageService()
+        output = MockRootViewModelOutput()
+        
+        
+        sut = RootViewModel(loginStorageService: loginStorageService,
+                            userService: userService)
+        
+        sut.output = output
+        try super.setUpWithError()
+        
+    }
+
+    override func tearDownWithError() throws {
+        sut = nil
+        userService = nil
+        loginStorageService = nil
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testShowLogin_whenLoginStorageReturnsEmptyUserToken() throws {
+     //given
+        loginStorageService.dict = [:]
+     //when
+        sut.startFlow()
+     //then
+        XCTAssertEqual(output.flow.count, 1)
+        XCTAssertEqual(output.flow[0], .login)
+    }
+}
+
+class MockLoginStorageService:LoginStorageService {
+    
+    var accessTokenKey: String {
+        return "ACCESS_TOKEN"
+    }
+    
+    var dict:[String:String] = [:]
+    
+    var setUserAccessTokenArray:[String] = []
+    func setUserAccessToken(value: String) {
+        dict[accessTokenKey] = value
+    }
+    
+    var getUserAccessTokenMock:String?
+    func getUserAccessToken() -> String? {
+        return dict[accessTokenKey]
+    }
+    
+        
+}
+
+class MockRootViewModelOutput:RootViewModelOutput {
+    
+    enum Flow {
+        case login
+        case mainApp
+    }
+    
+    var flow:[Flow] = []
+    
+    func showLogin() {
+        flow.append(.login)
+    }
+    
+    func showMainApp(userService: UserService) {
+        flow.append(.mainApp)
+    }
+    
+    
+}
+
+
+
+
