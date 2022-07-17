8337920c86a72dc53bc5a64853b67b00a1168728
Bri <sangheon0724@gmail.com>
2022-07-17 18:18:30 +0900

    done: END

---
+++ ViewModelType/Pods/Target Support Files/Pods-ViewModelType-ViewModelTypeUITests/Pods-ViewModelType-ViewModelTypeUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ViewModelType_ViewModelTypeUITests : NSObject
+@end
+@implementation PodsDummy_Pods_ViewModelType_ViewModelTypeUITests
+@end
--- ViewModelType/ViewModelType/ViewController.swift
+++ ViewModelType/ViewModelType/ViewController.swift
@@ -15,16 +15,8 @@ final class ViewController: UIViewController {
     @IBOutlet weak var countBtn: UIButton!
     
     let disposeBag = DisposeBag()
-    let viewModel: ViewModel
+    let viewModel = ViewModel(dependency: .init(dummy: 0))
     
-    init(viewModel: ViewModel) {
-        self.viewModel = ViewModel(dependency: .init(dummy: 0))
-        super.init(nibName: nil, bundle: nil)
-    }
-    
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
     override func viewDidLoad() {
         super.viewDidLoad()
         setUpBindings()
@@ -40,9 +32,6 @@ final class ViewController: UIViewController {
             .map { String($0) }
             .drive(countLabel.rx.text)
             .disposed(by: disposeBag)
-        
-  
-        
     }
 }
 
