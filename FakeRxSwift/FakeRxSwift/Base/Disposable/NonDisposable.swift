88e9d317c50ae294e98d1071c2c68b2663de5d7a
sangheon <sanghun0724@naver.com>
2022-08-13 12:45:06 +0900

    feat : add NonDIsposable

---
+++ FakeRxSwift/FakeRxSwift/Base/Disposable/NonDisposable.swift
@@ -0,0 +1,12 @@
+//
+//  NonDisposable.swift
+//  FakeRxSwift
+//
+//  Created by sangheon on 2022/08/13.
+//
+
+import Foundation
+
+final class NonDisposable: Disposable {
+    func dispose() { }
+}
